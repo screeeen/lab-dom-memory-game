dd608e5b23cb0990c736b50417342dbcbca15b29
screeeen <miguel@screenimplosion.com>
2019-04-11 10:47:01 +0200

    for revision

--- src/main.js
+++ src/main.js
@@ -1,60 +1,64 @@
-'use strict';
+"use strict";
 
-function main () {
+function main() {
   var memoryGame = new MemoryGame(cards);
-  var html = '';
-  memoryGame.cards.forEach(function (pic) {
-    html += '<div class="card" data-card-name="'+ pic.name +'">';
-    html += '  <div class="back" name="'+ pic.img +'"></div>';
-    html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
-    html += '</div>';
+  var html = ""; //document.querySelector('html');
+
+  memoryGame.cards.forEach(function(pic) {
+    html += '<div class="card" data-card-name="' + pic.name + '">';
+    html += '  <div class="back" name="' + pic.img + '"></div>';
+    html +=
+      '  <div class="front" style="background: url(img/' +
+      pic.img +
+      ') no-repeat"></div>';
+    html += "</div>";
   });
 
   // Add all the div's to the HTML
-  var memoryBoard = document.querySelector('#memory_board');
-  if(memoryBoard) { // this condition is for passing the tests
+  var memoryBoard = document.querySelector("#memory_board");
+  if (memoryBoard) {
+    // this condition is for passing the tests
     memoryBoard.innerHTML = html;
   }
-  
+
   // You will need to do something to the front as well
-  var front = document.querySelectorAll('.front');
+  var front = document.querySelectorAll(".front");
 
   // Bind the click event of each element to a function
-  var back = document.querySelector('.back');
-  back.addEventListener('click', function () {
-    // TODO: Your code goes here!
+  var back = document.querySelectorAll(".back");// returns node list
+  console.log(back);
+  back.forEach(card => {
+    console.log("card assigning listener " + card);
+    card.addEventListener("click", displayClickedCard);
   });
-  
-  
-  
-  // Helpers to create the logic of the game
-  function turnBackCards() {
-    setTimeout(function () {
-      memoryGame.pickedCards[0].style.background = '#456783';
-      memoryGame.pickedCards[1].style.background = '#456783';
-      memoryGame.pickedCards[0].classList.remove('active');
-      memoryGame.pickedCards[1].classList.remove('active');
-      prepareNextTurn();
-    }, 1000);
-  }
-  
-  function prepareNextTurn() {
-    memoryGame.pickedCards = [];
-    back.forEach((element)=> element.classList.remove('blocked'))
-    front.forEach((element)=> element.classList.remove('blocked'))
-  }
-  
-  function printGameInfo() {
-    document.getElementById('pairs_clicked').innerHTML = memoryGame.pairsClicked;
-    document.getElementById('pairs_guessed').innerHTML = memoryGame.pairsGuessed;
-  }
-  
-  function displayClickedCard(card) {
-    card.className += ' active';
-    card.style.background = 'url(img/' + card.getAttribute('name') + ') no-repeat';
-  }
-};
+}
 
-window.addEventListener('load', main);
+// Helpers to create the logic of the game
+function turnBackCards() {
+  setTimeout(function() {
+    memoryGame.pickedCards[0].style.background = "#456783";
+    memoryGame.pickedCards[1].style.background = "#456783";
+    memoryGame.pickedCards[0].classList.remove("active");
+    memoryGame.pickedCards[1].classList.remove("active");
+    prepareNextTurn();
+  }, 1000);
+}
 
+function prepareNextTurn() {
+  memoryGame.pickedCards = [];
+  back.forEach(element => element.classList.remove("blocked"));
+  front.forEach(element => element.classList.remove("blocked"));
+}
 
+function printGameInfo() {
+  document.getElementById("pairs_clicked").innerHTML = memoryGame.pairsClicked;
+  document.getElementById("pairs_guessed").innerHTML = memoryGame.pairsGuessed;
+}
+
+function displayClickedCard(card) {
+  card.className += " active";
+  card.style.background =
+    "url(img/" + card.getAttribute("name") + ") no-repeat";
+}
+
+window.addEventListener("load", main);
--- src/memory.js
+++ src/memory.js
@@ -1,11 +1,42 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pairsClicked = 0;
+  this.pickedCards = [];
+  this.pairsGuessed = 0;
 };
 
-MemoryGame.prototype.shuffleCards = function () {
+MemoryGame.prototype.shuffleCards = function (array) {
+  var m = array.length, t, i;
+  // While there remain elements to shuffle…
+  while (m) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+
+  return array;
+  
+  //inhouse shuffle
+  // let result = [];
+  // for (; array.length > 0; ) {
+  //   let randomNumber = Math.floor(Math.random() * array.length);
+  //   let randomCard = array.splice(randomNumber, 1);
+
+  //   result.push(randomCard[0]);
+  // }
+  // console.log(result);
+  // return result;
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked +=1;
+  let isMatch = firstCard.dataset.data-card-name === secondCard.dataset.data-card-name;
+console.log(isMatch + "    "+ firstCard.dataset);
 }
 
 MemoryGame.prototype.isFinished = function () {
